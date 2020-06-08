560602b9a3cf41a436a352e96a238cebc138c062
wanglong8767 <wanglong8767@yundasys.com>
2020-06-08 17:18:02 +0800

    first commit

---
+++ src/main/java/com/cadmin/cadmin/CadminApplication.java
@@ -0,0 +1,13 @@
+package com.cadmin.cadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cadmin/cadmin/CadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadmin.cadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
